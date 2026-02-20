f8ed70de1ed5e751321bbfe56a37536b8d3b5af8
Priyanshu Negi <npriyanshu63@gmail.com>
2026-02-20 16:27:51 +0530

    feat: Initialize NestJS application with CORS, global validation, Swagger documentation, and Morgan logging.

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,10 @@ async function bootstrap() {
 
   // 1. CORS Configuration
   app.enableCors({
-    origin: [process.env.FRONTEND_URL , "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
   });
 
   // 2. Global Validation Pipes
