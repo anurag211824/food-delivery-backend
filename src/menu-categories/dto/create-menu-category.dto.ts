65795f12680779550127b41475b079763f664633
Priyanshu Negi <npriyanshu63@gmail.com>
2026-03-23 13:22:36 +0530

    feat: menu category  dto update with image and  type

--- src/menu-categories/dto/create-menu-category.dto.ts
+++ src/menu-categories/dto/create-menu-category.dto.ts
@@ -1,9 +1,21 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsOptional, IsEnum } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
+import { VegType } from '@prisma/client';
 
 export class CreateMenuCategoryDto {
   @ApiProperty({ example: 'Main Course', description: 'Name of the menu category' })
   @IsString()
   @IsNotEmpty()
   name: string; // e.g., "Recommended", "Main Course", "Drinks"
+
+  @ApiProperty({ example: 'https://example.com/category-image.jpg', description: 'Image URL for the menu category' })
+  @IsString()
+  @IsOptional()
+  image?: string;
+
+
+  @ApiProperty({ example: 'VEG', description: 'VegType of the menu category' })
+  @IsEnum(VegType)
+  @IsOptional()
+  type?: VegType
 }
