dc666c9f09c8fb1f58d0febeb2bdf4bc8a920e06
Priyanshu Negi <npriyanshu63@gmail.com>
2026-04-15 16:25:19 +0530

    feat: add admin web app origin

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   // 1. CORS Configuration
   app.enableCors({
-    origin: [process.env.CUSTOMER_APP_ORIGIN, "http://localhost:5173", process.env.RESTAURANT_APP_ORIGIN,process.env.RIDER_APP_ORIGIN],
+    origin: [process.env.CUSTOMER_APP_ORIGIN, "http://localhost:5173", process.env.RESTAURANT_APP_ORIGIN,process.env.RIDER_APP_ORIGIN,process.env.ADMIN_WEB_APP_ORIGIN],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
